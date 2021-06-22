fc22b31bb81ee4e14de614c9aef3669092cbd1f2
Ruslan Petrovich <60133073+rkomehdaht@users.noreply.github.com>
2021-06-22 22:10:10 +0300

    Initial commit

---
+++ GP.ps1
@@ -0,0 +1,5 @@
+﻿New-ADOrganizationalUnit -Name "Command" -Path "DC=komehdaht,DC=local"
+New-ADUser komehdaht
+Move-ADObject -Identity "CN=komehdaht,CN=Users,DC=komehdaht,DC=local" -TargetPath "OU=Command,DC=komehdaht,DC=local"
+New-GPO -Name FIRSTGPO | New-GPLink -Target "OU=Command,DC=komehdaht,DC=local"
+Set-GPLink -Name FIRSTGPO -Target "OU=Command,DC=komehdaht,DC=local" -LinkEnabled Yes
