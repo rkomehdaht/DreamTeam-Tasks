9d5d046420acf08f8f43d024222a7e4f8c71d719
balovich78 <83667087+balovich78@users.noreply.github.com>
2021-06-25 15:16:40 +0300

    Add files via upload

---
+++ sozdanieAD.ps1
@@ -0,0 +1,14 @@
+﻿# ustanovka AD 
+# prosmotr vsex vozmoznix variantov/spisok
+get-windowsfeature
+
+# ustanovka AD komandoi
+install-windowsfeature AD-Domain-Services
+
+# sozdaybt Web-Server
+install-windowsfeature Web-Server
+
+
+
+#proverka pabotosposobnosni AD
+Get-ADUser -Identity администратор -Properties *
