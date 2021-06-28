618434174f25de446e71740ff228b1f1db3055d8
Oleg Novitsky <83610833+Oleg1996Novitsky@users.noreply.github.com>
2021-06-28 10:29:05 +0300

    Add files via upload

---
+++ dns.ps1
@@ -0,0 +1,10 @@
+﻿#поднятие DNS
+Install-WindowsFeature DNS -IncludeManagementTool
+
+#создание записи
+Add-DnsServerResourceRecordA -Name myrecord -IPv4Address 1.2.3.4 -ZoneName pc1.local 
+
+#тесты
+Test-DnsServer 172.20.10.2
+Get-DnsServerDiagnostics
+Get-DnsServerZoneScope pc1.local
