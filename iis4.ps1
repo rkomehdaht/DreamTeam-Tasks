1c1da9a9f615f8b75a73b07931f55c5d81906b97
Sergeybon <83625978+Sergeybon@users.noreply.github.com>
2021-06-30 06:15:17 +0300

    Update iis4.ps1

--- iis4.ps1
+++ iis4.ps1
@@ -1,30 +1,28 @@
 ﻿# install IIS and subfeatures
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
-
-#check IIS
+# check IIS 
+While ((Get-WindowsFeature Web-Server).InstallState -eq "Installed")
 if ((Get-WindowsFeature Web-Server).InstallState -eq "Installed") {
     Write-Host "IIS is installed on $vm"
 } 
 else {
-
     Write-Host "IIS is not installed on$vm"
 
 # Wait for IIS installing
 Wait-Event -Timeout 30
 }
 
-
 #Import-Module
 Import-Module -Name WebAdministration
 
 <#  picture
-1) open picture from internet
+1) open picture from internet 
 2) change picture in destination folder
 #>
   $url = "https://lifeandjoy.ru/uploads/posts/2014-07/1406568288_lifrandjoy.ru_26.jpg"
 
    Invoke-WebRequest -Uri $url -OutFile "C:\inetpub\wwwroot\iisstart.png"
    
-   #open site
+   # open site 
    Start "http://localhost/"
